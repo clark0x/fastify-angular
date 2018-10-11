e52ac5bf32793f8f003b30f54013a2a691eabbd3
Clark Wang <clark@bitkan.com>
2018-10-11 21:19:30 +0800

    fix(ssr): fix `/` doesn't render by server side

--- packages/fastify-angular/plugins/browser.js
+++ packages/fastify-angular/plugins/browser.js
@@ -1,6 +1,11 @@
 'use strict';
 
 module.exports = function fastifyAngularBrowser(fastify, opts, next) {
-  fastify.register(require('fastify-static'), { prefix: '/', root: opts.browser });
+  fastify.register(require('fastify-static'), {
+    prefix: '/',
+    root  : opts.browser,
+    // By default, send support `index.html` files. We have to disable this feature while avoid response 403
+    index : '<ignore>',
+  });
   next();
 };
