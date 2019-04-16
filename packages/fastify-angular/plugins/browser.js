e0e73ad56fc2197fad1228378c4492dc3b15811b
Clark Wang <clark@bitkan.com>
2019-04-17 00:02:58 +0800

    allow access dir

--- packages/fastify-angular/plugins/browser.js
+++ packages/fastify-angular/plugins/browser.js
@@ -4,6 +4,7 @@ module.exports = function fastifyAngularBrowser(fastify, opts, next) {
   fastify.register(require('fastify-static'), {
     prefix: '/',
     root  : opts.browser,
+    redirect: true,
     // By default, send support `index.html` files. We have to disable this feature while avoid response 403
     index : '<ignore>',
   });
